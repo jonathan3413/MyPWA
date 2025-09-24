575e200896b945fd76a14322c7254a5ccd02c6c6
excaa <156346013+jonathan3413@users.noreply.github.com>
2025-09-24 21:23:48 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,21 @@
+const CACHE_NAME = "gabut-app-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("pwa-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
+  console.log("Service Worker: Installed");
 });
 
 self.addEventListener("fetch", event => {
